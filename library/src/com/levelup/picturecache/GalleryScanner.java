648676c51219dfbe00b8304b555a810aba310615
Steve Lhomme <robux4@gmail.com>
2013-01-08 23:44:06 +0100

    use our own GalleryScanner

---
+++ library/src/com/levelup/picturecache/GalleryScanner.java
@@ -0,0 +1,36 @@
+package com.levelup.picturecache;
+
+import java.io.File;
+
+import android.content.Context;
+import android.media.MediaScannerConnection;
+import android.media.MediaScannerConnection.MediaScannerConnectionClient;
+import android.net.Uri;
+
+public class GalleryScanner implements MediaScannerConnectionClient {
+	/**
+	 * 
+	 */
+	MediaScannerConnection mScanner;
+	File mDst;
+	//CountDownLatch mLock;
+	
+	public GalleryScanner(Context context) {
+		mScanner = new MediaScannerConnection(context, this);
+	}
+	
+	public void scan(File dst) {
+		mDst = dst;
+		//mLock = new CountDownLatch(1);
+		mScanner.connect();
+	}
+
+	public void onMediaScannerConnected() {
+		mScanner.scanFile(mDst.getAbsolutePath(), null);
+	}
+
+	public void onScanCompleted(String path, Uri uri) {
+		mScanner.disconnect();
+		//mLock.countDown();
+	}
+}
